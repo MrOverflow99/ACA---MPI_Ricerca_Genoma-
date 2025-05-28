b8181c20697537e4623c0584f8ebc6e914e54fe2
MrOverflow99 <michele.difriscoramire01@universitadipavia.it>
2025-05-28 12:48:32 +0200

    First commit

---
+++ MPI/ACA/Libraries/hash_funct.c
@@ -0,0 +1,21 @@
+//
+// Created by michele and marco on 5/28/25.
+//
+
+#include "hash_funct.h"
+
+long long int FNV_hash(char *pattern){
+  return 0;
+}
+
+long long int Murmur_hash(char *pattern){
+  return 0;
+}
+
+long long int CRC32_hash(char *pattern){
+  return 0;
+}
+
+long long int xxHash(char *pattern){
+  return 0;
+}
---
+++ Serial/ACA/hash_funct.h
@@ -0,0 +1,18 @@
+//
+// Created by michele and marco on 5/28/25.
+//
+
+#ifndef HASH_FUNCT_H
+#define HASH_FUNCT_H
+
+#include "hash_funct_h"
+#include <string.h>
+#include <math.h>
+#include <limits.h>
+
+long long int FNV_hash(char *pattern);
+long long int Murmur_hash(char *pattern);
+long long int CRC32_hash(char *pattern);
+long long int xxHash(char *pattern);
+
+#endif //HASH_FUNCT_H
